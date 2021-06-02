ea8cc84ddc4784d5ddf3278a652cab0aa0589c86
Karan <76738681+K4RAN06@users.noreply.github.com>
2021-06-02 10:22:36 -0400

    Delete __resource.lua

--- __resource.lua
+++
@@ -1,9 +0,0 @@
-fx_version 'cerulean'
-games {'gta5'}
-
-author 'Karan#6101, Whoeza#0319, Juice#0886'
-description 'Make Your Ped Suicide By "/suicide" in chat'
-version '1.0.0'
-
-
-client_script 'client.lua'
